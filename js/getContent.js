137ee75bf0e3b6923f7a9a30021d3751a21f0bde
paulo.assis <paulo_hda@hotmail.com>
2020-12-10 06:12:02 -0300

    Exemplo node path

--- js/getContent.js
+++ js/getContent.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const content = fs.readFileSync(path.resolve(__dirname, '../conteudo/content/content.html'),'utf-8');
+const content = fs.readFileSync(path.resolve(__dirname, '../projeto2/content/content.html'),'utf-8');
 
 const area = document.querySelector('.area');
 
